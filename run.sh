3206023f5932960ca72f61c264754da273dfb19a
Sherman Chen <schen@simviu.com>
2023-06-26 13:44:47 +0800

    test run on dev

---
+++ run.sh
@@ -0,0 +1,2 @@
+scripts/aanet+_predict.sh 
+
--- scripts/aanet+_predict.sh
+++ scripts/aanet+_predict.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 # Predict
+
+MODEL=pretrained/aanet+_sceneflow-d3e13ef0.pth
+#MODEL=pretrained/aanet+_kitti15-2075aea1.pth
+IMG_DIR=rund
+
+
 CUDA_VISIBLE_DEVICES=0 python predict.py \
---data_dir demo \
---pretrained_aanet pretrained/aanet+_kitti15-2075aea1.pth \
+--data_dir rund \
+--pretrained_aanet $MODEL \
 --feature_type ganet \
 --feature_pyramid \
 --refinement_type hourglass \
